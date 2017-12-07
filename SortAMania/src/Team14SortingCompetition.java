a318969a587a71bafefc33338feeaf1ec82ebe03
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-07 13:13:19 -0500

    sort

--- SortAMania/src/Team14SortingCompetition.java
+++ SortAMania/src/Team14SortingCompetition.java
@@ -7,6 +7,7 @@ public class Team14SortingCompetition extends SortCompetition {
 		Team14SortingCompetition bot = new Team14SortingCompetition();
 		int[] list = new int[10000];
 		int[][] list2 = new int[1000][1000];
+		Comparable[] list3 = new Comparable[generateNum()];
 		generateNum(list2);
 		//generateNum(list);
 		bubbleSort(list);
@@ -14,6 +15,7 @@ public class Team14SortingCompetition extends SortCompetition {
 		//System.out.println(ChallangeOne(list));
 		//System.out.println(Arrays.deepToString(list2));
 		System.out.println(bot.challengeFour(list2));
+		System.out.println(bot.challengeFive(list3,5));
 	}
 	public int challengeOne(int[] arr) {
 		//bubbleSort(list);
@@ -55,8 +57,11 @@ public class Team14SortingCompetition extends SortCompetition {
 	public int challengeFive(Comparable[] arr, Comparable query) {
 		bubbleSort(arr);
 		for(int i = 0; i < arr.length; i++) {
-			
+			if(arr[i] == query) {
+				return i;
+			}
 		}
+		return -1;
 	}
 	
 	public static void bubbleSort(int[]list1) {
@@ -82,11 +87,29 @@ public class Team14SortingCompetition extends SortCompetition {
 		}
 	}
 	
+	public static void bubbleSort(Comparable[]list1) {
+		for(int i = 0; i < list1.length-1; i++) {
+			for(int j = i+1; j < list1.length; j++) {
+				if(list1[i].compareTo(list1[j]) < 0) {
+					Comparable temp = list1[j];
+					list1[j] = list1[i];
+					list1[i] = temp;
+				}
+			}
+		}
+	}
+	
 	public static void printIntArray(int[] arr) {
 		for(int i = 0; i < arr.length; i++) {
 			System.out.print("[" + arr[i] + "] ");
 		}
 		System.out.println();
+	}
+	
+	public static int generateNum(Comparable) {
+		Random rand = new Random();
+		int  n = rand.nextInt(10000) + 1;
+		return n;
 	}
 	
 	public static void generateNum(int[]list) {
