febf63d69a44e9899015d8880cbecac9c01405db
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-12-07 12:07:46 -0500

    Team 9

---
+++ SortAMania/src/Team9SortCompetition.java
@@ -0,0 +1,39 @@
+
+public class Team9SortCompetition extends SortCompetition {
+
+	@Override
+	public int challengeOne(int[] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeTwo(String[] arr, String query) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeThree(int[] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeFour(int[][] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeFive(Comparable[] arr, Comparable query) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String greeting() {
+		return "team numba 9 reportin fer dooty";
+	}
+		
+}
