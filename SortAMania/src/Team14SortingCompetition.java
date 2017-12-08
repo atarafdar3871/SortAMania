ac25493f97404e7058b2058aa0303958f7fb761c
Jacky <jlin3676@bths.edu>
2017-12-07 21:59:32 -0500

    o

--- SortAMania/src/Team14SortingCompetition.java
+++ SortAMania/src/Team14SortingCompetition.java
@@ -7,21 +7,17 @@ public class Team14SortingCompetition extends SortCompetition {
 		Team14SortingCompetition bot = new Team14SortingCompetition();
 		int[] list = new int[10000];
 		int[][] list2 = new int[1000][1000];
-		Comparable[] list3 = new Comparable[generateNum()];
 		generateNum(list2);
-		//generateNum(list);
-		bubbleSort(list);
-		//printIntArray(list);
+		generateNum(list);
+		printIntArray(list);
 		//System.out.println(ChallangeOne(list));
 		//System.out.println(Arrays.deepToString(list2));
+		System.out.println(bot.challengeThree(list));
 		System.out.println(bot.challengeFour(list2));
-		System.out.println(bot.challengeFive(list3,5));
+		//System.out.println(bot.challengeFive(list3,5));
 	}
 	public int challengeOne(int[] arr) {
-		//bubbleSort(list);
-		if(arr.length % 2 == 1) {
-			return arr[arr.length/2];
-		}
+		bubbleSort(arr);
 		return median(arr);
 	}
 	public int challengeTwo(String[] arr, String query) {
@@ -39,8 +35,18 @@ public class Team14SortingCompetition extends SortCompetition {
 		}
 	
 	public int challengeThree(int[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int[] arr1 = new int[arr.length/4];
+		for(int i = 0; i < arr1.length; i++) {
+			arr1[i] = arr[i];
+			//System.out.println(arr[i]);
+		}
+		for(int j = 0; j < arr1.length; j++) {
+			arr[j] = arr1[j];
+		}
+		bubbleSort(arr);
+		//printIntArray(arr1);
+		return median(arr);
 	}
 	
 	public int challengeFour(int[][] arr) {
@@ -55,13 +61,80 @@ public class Team14SortingCompetition extends SortCompetition {
 	
 	
 	public int challengeFive(Comparable[] arr, Comparable query) {
-		bubbleSort(arr);
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] == query) {
+		mergeSort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==query)
+			{
 				return i;
 			}
 		}
 		return -1;
+	}
+	
+	public static Comparable[] merge(Comparable[] list1, Comparable[] list2)
+	{
+		Comparable[] merged = new Comparable[list1.length + list2.length];
+		int z = 0;
+		int firstList = 0;
+		int secList = 0;
+		while(firstList < list1.length && secList < list2.length)
+		{
+			if (list1[firstList].compareTo(list2[secList]) == 0)
+			{
+				merged[z] = list1[firstList];
+				firstList++;
+				secList++;
+				z++;
+			}
+			else if (list1[firstList].compareTo(list2[secList]) < 0)
+			{
+				merged[z] = list1[firstList];
+				firstList++;
+				z++;
+			}
+			else if (list2[secList].compareTo(list1[firstList]) < 0)
+			{
+				merged[z] = list2[secList];
+				secList++;
+				z++; 
+			}
+		}
+		
+		if (firstList >= list1.length)
+		{
+			while (secList < list2.length)
+			{
+				merged[z] = list2[secList];
+				z++;
+				secList++;
+			}
+		}
+		if(secList >= list2.length)
+		{
+			while (firstList < list1.length)
+			{
+				merged[z] = list1[firstList];
+				z++;
+				firstList++;
+			}
+		}
+		return merged;
+	}
+	
+	public static Comparable[] mergeSort(Comparable[] list)
+	{
+		if (list.length == 1)
+		{
+			return list;
+		}
+		else
+		{
+			int half = list.length/2;
+			Comparable[] firstHalf = Arrays.copyOfRange(list, 0, half);
+			Comparable[] secondHalf = Arrays.copyOfRange(list, half, list.length);
+			return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+		}
 	}
 	
 	public static void bubbleSort(int[]list1) {
@@ -87,15 +160,15 @@ public class Team14SortingCompetition extends SortCompetition {
 		}
 	}
 	
-	public static void bubbleSort(Comparable[]list1) {
-		for(int i = 0; i < list1.length-1; i++) {
-			for(int j = i+1; j < list1.length; j++) {
-				if(list1[i].compareTo(list1[j]) < 0) {
-					Comparable temp = list1[j];
-					list1[j] = list1[i];
-					list1[i] = temp;
-				}
+	public static void insertionSort(int[]list1) {
+		for(int i = 0; i < list1.length - 1; i++) {
+			int num = list1[i];
+			int j = i-1;
+			while(j >= 0 && list1[j] > num) {
+				list1[j+1] = list1[j];
+				j = j-1;
 			}
+			list1[j+1] = num;
 		}
 	}
 	
@@ -106,10 +179,10 @@ public class Team14SortingCompetition extends SortCompetition {
 		System.out.println();
 	}
 	
-	public static int generateNum(Comparable) {
-		Random rand = new Random();
-		int  n = rand.nextInt(10000) + 1;
-		return n;
+	public static int generateNum() {
+			Random rand = new Random();
+			int  n = rand.nextInt(10000) + 1;
+			return n;
 	}
 	
 	public static void generateNum(int[]list) {
@@ -131,8 +204,7 @@ public class Team14SortingCompetition extends SortCompetition {
 		//System.out.println("finished");
 	}
 	public String greeting() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Hello";
 	}
 	
 	public static int median(int[]arr) {
